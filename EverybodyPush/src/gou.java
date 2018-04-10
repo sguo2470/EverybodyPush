279d4760297d144dd38676424a8518a14f1b85e0
ejw5285 <ejw5285@E165-WL30.up.ist.local>
2018-04-10 14:33:07 -0400

    added class gou

---
+++ EverybodyPush/src/gou.java
@@ -0,0 +1,14 @@
+
+public class gou {
+    private String name;
+    
+    public gou(String name){
+       this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+}
