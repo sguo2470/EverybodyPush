a19d2975eb13959a19b91b87a8336b7624c7ad98
svg5502 <svg5502@E165-WL29.up.ist.local>
2018-04-10 14:14:41 -0400

    Created project.

---
+++ src/everybodypush/EverybodyPush.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package everybodypush;
+
+/**
+ *
+ * @author svg5502
+ */
+public class EverybodyPush {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
